ea174d936355b46cda98d837dec250901318c100
Dez <dez@dezs-mbp.attlocal.net>
2021-02-28 10:59:50 -0500

    searchbar setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
-
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
   return (
     <HeaderContainer>
@@ -11,9 +12,14 @@ function Header() {
         <HeaderAvatar
         //TODO: ADD onclick with src
         />
+        <AccessTimeIcon />
       </HeaderLeft>
 
       {/* Header Search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder="Search" />
+      </HeaderSearch>
       {/* Header Right */}
     </HeaderContainer>
   );
@@ -21,13 +27,52 @@ function Header() {
 
 export default Header;
 
-const HeaderContainer = styled.div`
-  > h2 {
-    color: red;
+const HeaderRight = styled.div``;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
   }
 `;
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
 
-const HeaderLeft = styled.div``;
-const HeaderAvatar = styled(Avatar)``;
-const HeaderSearch = styled.div``;
-const HeaderRight = styled.div``;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
